237347ffe380d7641d0a0ce30047ead241e76777
ardavis414 <151779906+ardavis414@users.noreply.github.com>
2024-05-19 16:25:50 -0400

    navigation-js updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
 
-var burgerButton = document.querySelector("burger-button");
+// my selector for the burger button
+var burgerButton = document.querySelector("#burger-button");
+var mainNav = document.querySelector("#main-nav");
 
-burgerButton.addEventListener("click", () =>{
-    console.log("botton working");
+var NavEstaVisible = false;
+
+burgerButton.addEventListener("click", () => {
+    // console.log("botton working");
+
+    if (NavEstaVisible === false) {
+        mainNav.classList.remove("hide-menu");
+        NavEstaVisible = true;
+    }else{
+        mainNav.classList.add("hide-menu");
+        NavEstaVisible = false;
+    }
 })
+
+mainNav.addEventListener("click", () => {
+    mainNav.classList.add("hide-menu");
+    NavEstaVisible = false;
+})
+
+
+
